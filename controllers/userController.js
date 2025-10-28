0fdec819463cdf8e9f8adafd713d4c8ae9dbc81a
MuhammadWildanf <wildanf.daftar@gmail.com>
2025-10-28 13:51:43 +0700

    update form & button

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,24 @@
 const { db } = require("../config/firebase");
-const { uploadBase64Image } = require("../utils/uploadBase64");
+// const { uploadBase64Image } = require("../utils/uploadBase64");
 
 exports.saveUser = async (req, res) => {
   try {
-    const { nama, foto } = req.body;
-    if (!nama || !foto) {
-      return res.status(400).json({ message: "Nama dan foto wajib diisi." });
+   const { nama, email } = req.body;
+   if (!nama || !email) {
+      return res.status(400).json({ success: false, message: "Nama dan email wajib diisi." });
     }
 
-    const fotoUrl = await uploadBase64Image(foto);
+    // const fotoUrl = await uploadBase64Image(foto);
     const newRef = db.ref("users").push();
 
     await newRef.set({
       nama,
-      foto: fotoUrl,
+       email,
+      // foto: fotoUrl,
       timestamp: Date.now(),
     });
 
-    res.json({ success: true, message: "Data berhasil disimpan!", fotoUrl });
+    res.json({ success: true, message: "Data berhasil disimpan!"});
   } catch (error) {
     console.error("❌ Error saveUser:", error);
     res.status(500).json({ message: "Terjadi kesalahan server" });
